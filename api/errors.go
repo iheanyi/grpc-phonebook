a155da393ebc4d8f04695656f72036d7feab5bbc
Iheanyi Ekechukwu <iheanyi@digitalocean.com>
2017-08-25 17:52:10 -0400

    Finally get all imports working.

--- api/errors.go
+++ api/errors.go
@@ -1,11 +1,11 @@
 package api
 
 import (
-	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 var (
-	ErrorNotFound         = grpc.Errorf(status.NotFound, "Contact not found")
-	ErrorDuplicateContact = grpc.Errorf(status.AlreadyExists, "Contact already exists")
+	ErrorNotFound         = status.Errorf(codes.NotFound, "Contact not found")
+	ErrorDuplicateContact = status.Errorf(codes.AlreadyExists, "Contact already exists")
 )
