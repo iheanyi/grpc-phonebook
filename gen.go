835834cc41e32e5343f3dd22c7971310edf89113
Iheanyi Ekechukwu <iheanyi@digitalocean.com>
2017-08-25 18:05:04 -0400

    Remove gen.go

--- gen.go
+++
@@ -1,3 +0,0 @@
-package grpc_phonebook
-
-//go:generate protoc -I=. --js_out=import_style=commonjs,binary:. --go_out=plugins=grpc,import_path=$GOPATH/src/github.com/iheanyi/grpc-phonebook:. phonebook.proto
